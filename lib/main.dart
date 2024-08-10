dbd983418c923830ce540667660c1aa9b9c7c82e
yuwenwenchiu <sharon830316@gmail.com>
2024-08-10 23:39:07 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  firstVerse();
+  bridge();
+  chorus();
+  secondVerse();
+  bridge();
+  chorus();
+  outro();
+}
+
+void firstVerse() {
+  print('倔強的表情 就這樣隔離了我們想觸摸的臉龐 不再體貼退讓');
+  print('沉默的對話 就這樣取代了我們想聆聽的願望 不再交換悲傷');
+  print('太自以為的世界');
+}
+
+void bridge() {
+  print('被你離開的反作用力 鎖進黑暗陷阱');
+}
+
+void chorus() {
+  print('一樣的月光 怎麼看得我越來越心慌');
+  print('你留下最清楚的步伐 竟是指引我孤單的方向');
+  print('一樣的月光 怎麼照不亮未來的形狀');
+  print('喔 就這樣吧 我的愛 讓寂寞的月光佔據我的窗');
+}
+
+void secondVerse() {
+  print('自由的路況 就這樣阻擋了我們想超速的逃亡 不再戒慎緊張');
+  print('獨處的時光 就這樣取代了我們想搜集的擁抱 不再殷切需要');
+  print('太多理智的世界');
+}
+
+void outro() {
+  print('一樣的月光 其實看得我越來越心慌');
+  print('怎麼你留下最真實的回憶 都是提醒我失去的聲音');
+  print('一樣的月光 已經照不亮未來的形狀');
+  print('喔 能回來嗎 我的愛 讓瘋狂的愛情佔據我的窗');
+}
